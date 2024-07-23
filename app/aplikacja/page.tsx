ddf7a354bd3188fd535c05eb31b17064a44db93f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-07-23 12:21:44 +0200

    add container for aplikacja

--- app/aplikacja/page.tsx
+++ app/aplikacja/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Aplikacja = () => {
   return (
-    <div>
-        <h1 className="text-green-400">Aplikacja</h1>
+    <div className="container mx-auto px-4 min-h-screen flex flex-col border border-green-900">
+        <h1 className="border border-black">Aplikacja Test</h1>
     </div>
   )
 }
