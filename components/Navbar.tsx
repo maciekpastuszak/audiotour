3191e1281bf69d0ef6dc6f3bcb02cc0d3e36a554
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-07-24 14:42:18 +0200

    add mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import {useState} from 'react'
 import Image from 'next/image'
 
 const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
     <div className="bg-white shadow-md w-full">
       <div className="container mx-auto px-4 md:px-12 lg:px-24 py-3 flex justify-between items-center">
@@ -15,12 +23,27 @@ const Navbar = () => {
             <Link href='/kontakt'><p className="text-stone-900 hover:text-gray-500">Kontakt</p></Link>
         </div>
         <div className="md:hidden items-center">
-          <button className="text-stone-900 hover:text-gray-500">
-            <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16m-7 6h7'></path>
-            </svg>
+            <button onClick={toggleMenu} className="text-stone-900 hover:text-gray-500">
+            {isMenuOpen ? (
+              <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            ) : (
+              <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg>
+            )}
           </button>
         </div>
+        {isMenuOpen && (
+          <div className='flex flex-col md:hidden'>
+            <Link href='/audioguide'><p className="text-stone-900 hover:text-gray-500">Audioguide</p></Link>
+            <Link href='/przewodnik'><p className="text-stone-900 hover:text-gray-500">Przewodnik</p></Link>
+            <Link href='/oferta'><p className="text-stone-900 hover:text-gray-500">Oferta</p></Link>
+            <Link href='/portfolio'><p className="text-stone-900 hover:text-gray-500">Portfolio</p></Link>
+            <Link href='/kontakt'><p className="text-stone-900 hover:text-gray-500">Kontakt</p></Link>
+          </div>
+        )}
       </div>
     </div>
   )
