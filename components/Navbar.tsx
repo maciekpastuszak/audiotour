3b0a2ea7bbdf2ff1700e95c77ac87abd1c179a12
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-07-24 11:31:52 +0200

    modify logo and menu in navbar lg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className="w-4/5 mx-auto justify-center items-center border border-neutral-900">
-        <ul className="flex flex-row justify-between items-center text-white">
-            <li><Link href='/audioguide' /> Audioguide</li>
-            <li><Link href='/przewodnik' /> Przewodnik turystyczny</li>
-            <li><Link href='/oferta' /> Oferta</li>
-            <li><Link href='/portfolio' /> Portfolio</li>
-            <li><Link href='/kontakt' />Kontakt</li>
-        </ul>
+    <div className="bg-white shadow-md w-full">
+      <div className="container mx-auto px-24 py-3 flex justify-between items-center">
+        <Image src="/img/logo.png" width={217} height={25} alt="logo" />
+        <div className="hidden md:flex space-x-8 md:space-x-16 font-halyard">
+            <Link href='/audioguide'><p className="text-stone-900 hover:text-gray-500">Audioguide</p></Link>
+            <Link href='/przewodnik'><p className="text-stone-900 hover:text-gray-500">Przewodnik</p></Link>
+            <Link href='/oferta'><p className="text-stone-900 hover:text-gray-500">Oferta</p></Link>
+            <Link href='/portfolio'><p className="text-stone-900 hover:text-gray-500">Portfolio</p></Link>
+            <Link href='/kontakt'><p className="text-stone-900 hover:text-gray-500">Kontakt</p></Link>
+        </div>
+      </div>
     </div>
   )
 }
