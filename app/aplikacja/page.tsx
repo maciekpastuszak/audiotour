6bb0eff13913fdcc9a29879249b7fcec9e856a72
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-07-28 16:20:34 +0200

    add shadcn button

--- app/aplikacja/page.tsx
+++ app/aplikacja/page.tsx
@@ -1,5 +1,6 @@
 import Carousel from '@/components/Carousel'
 import Navbar from '@/components/Navbar'
+import { Button } from '@/components/ui/button'
 import { appCarouselIcons } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
@@ -7,13 +8,13 @@ import React from 'react'
 const Aplikacja = () => {
   return (
       <section id='hero' className='w-full flex flex-col justify-center items-center bg-gray-200'>
-          <div className='w-3/4 md:w-1/2 mx-auto mt-8 md:mt-12'>
+          <div className='w-3/4 md:w-1/2 mx-auto mt-8 md:mt-12 text-center'>
             <Image src='/app-grid.png' alt='app-grid' layout='responsive' width={1} height={1} />
             <div className='my-6 text-center text-3xl md:text-4xl font-bold font-articulat'>
               <p>Every place an <span className='text-blue-500'>app</span></p>
               <p>200 <span className='text-xl font-normal'>wdrożeń w całej Polsce</span></p>
             </div>
-            
+            <Button className='bg-blue-500 rounded-full'>Nasze realizacje</Button>
           </div>
         <Carousel />
       </section>
