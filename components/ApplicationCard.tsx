dcf176f80972d4043176d4cdeca7a6f3ac3b5d59
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-07-30 23:35:40 +0200

    layout updates for ApplicationCard

--- components/ApplicationCard.tsx
+++ components/ApplicationCard.tsx
@@ -4,23 +4,24 @@ import { Button } from './ui/button'
 
 const ApplicationCard = () => {
   return (
-    <div className='relative items-start w-4/5 my-5 border border-violet-700'>
+    <div className='items-start w-5/6 my-5 bg-white min-w-[450px]'>
         <div className='flex ms-5 my-5'>
             <Image src="/img/app-icons/Icon Audioprzewodniki.svg" width={50} height={50 }alt="Ikona - Audioprzewodniki" />
             <div className='font-articulat leading-3'>
                 <h3 className='text-lg font-bold font-articulat'>Audioprzewodniki</h3>
-                <p>Prosta obsługa, <span className='text-blue-500'>szybka realizacja</span></p>
+                <p className='text-sm md:text-noraml'>Prosta obsługa, <span className='text-blue-500'>szybka realizacja</span></p>
             </div>
         </div>
-        <div className='bg-application-banner bg-cover'>
-            <h2 className='text-3xl text-white font-semibold mx-5 px-2 py-16'>Idealne rozwiązanie dla <br />muzeów oraz w przestrzeni <br /> miejskiej</h2>
-            <Button className='bg-white rounded-full text-black px-40 mx-7 mb-20'>Więcej</Button>
+        <div className='relative bg-application-banner bg-cover h-96'>
+            <h2 className='text-xl md:text-3xl text-white font-semibold mx-5 px-2 py-5 md:py-8 text-nowrap'>Idealne rozwiązanie dla <br />muzeów oraz w przestrzeni <br /> miejskiej</h2>
+            <Button className='absolute bg-white rounded-full text-black right-10 bottom-10 px-10 md:px-40 md:mx-7'>Więcej</Button>
+            <div className='absolute max-w-60 md:max-w-96 h-full top-28 left-5 md:top-2 md:-right-20'>
+                <img src='/img/app-phone-mockups/Banner Audioprzewodniki Tel.png' alt='Smartphone - Audioprzewodniki' />
+            </div>
         </div>
-        <div className='absolute max-w-96 h-full top-2 -right-28 border border-rose-700'>
-            <img src='/img/app-phone-mockups/Banner Audioprzewodniki Tel.png' alt='Smartphone - Audioprzewodniki' />
-        </div>
-        <div>
-            
+        <div className='w-3/5 m-8'>
+            <p className='pb-4'>Audioprzewodnik to audioprzewodnik, który oprowadzi Pańśtwa w multimedialny sposób po multimedialnym mieście Kraków.</p>
+            <p>Doskonały do multimedialnego zwiedzania audioprzewodnika w formie audioprzewodnika za pomocą aplikacji multimedialnej. Nie czekaj ani chwili dłużej, zamów audioprzewodnik w formie audioprzewonika już dziś!</p>
         </div>
     </div>
   )
