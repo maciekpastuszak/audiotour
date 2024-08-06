7462b69f86dab18a90171f241772bbd1a2eb772c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-08-06 14:34:55 +0200

    update animations in AppBannerCard

--- components/AppBannerBlue.tsx
+++ components/AppBannerBlue.tsx
@@ -4,10 +4,15 @@ import Image from 'next/image'
 import React from 'react'
 import { Button } from './ui/button'
 import { motion } from 'framer-motion'
+import { fadeIn } from '../utils/motion'
 
 const AppBannerBlue = () => {
   return (
-    <div className='items-start w-5/6 my-5 bg-white min-w-[375px] drop-shadow-md'>
+    <motion.div 
+    className='items-start w-5/6 my-5 bg-white min-w-[375px] drop-shadow-md'
+    variants={fadeIn('right', 'tween', 0.2, 1)}
+    initial="hidden"
+    whileInView="show">
         <div className='flex ms-5 my-3'>
             <Image src="/img/app-icons/Icon_Audioprzewodniki.svg" width={55} height={55}alt="Ikona - Audioprzewodniki" />
             <div className='font-articulat leading-3'>
@@ -15,23 +20,23 @@ const AppBannerBlue = () => {
                 <p className='text-sm md:text-noraml'>Prosta obsługa, <span className='text-blue-500'>szybka realizacja</span></p>
             </div>
         </div>
-        <div className={`relative bg-[url('/img/app-banners/Banner_Audioprzewodniki_Wide.png')] bg-cover h-80`}>
+        <div className={`relative w-full md:w-4/5 bg-[url('/img/app-banners/Banner_Audioprzewodniki_Wide.png')] bg-cover h-80`}>
             <h2 className='text-xl md:text-3xl text-white font-semibold mx-5 px-2 py-5 md:py-8 text-nowrap'>Idealne rozwiązanie dla <br />muzeów oraz w przestrzeni <br /> miejskiej</h2>
             <Button className='absolute bg-white rounded-full text-black right-10 md:left-0 md:w-[320px] bottom-10 md:bottom-24 px-10 md:mx-7'>Więcej</Button>
+        </div>
             <motion.div 
-                className='absolute max-w-48 md:max-w-80 lg:max-w-96 h-full top-28 md:-top-16 left-5 md:left-auto md:-right-10'
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ duration: 0.7 }}
+                className='absolute w-48 md:w-80 lg:w-[400px] h-full top-48 md:top-5 left-5 md:left-auto md:-right-20'
+                variants={fadeIn('left', 'tween', 0.4, 1)}
+                initial="hidden"
+                whileInView="show"
             >
                 <img src='/img/app-phone-mockups/Banner_Audioprzewodniki_Tel.png' alt='Smartphone - Audioprzewodniki' />
             </motion.div>
-        </div>
         <div className='md:w-3/5 m-8 pt-4 text-justify md:text-left'>
             <p className='pb-4'>Audioprzewodnik to audioprzewodnik, który oprowadzi Pańśtwa w multimedialny sposób po multimedialnym mieście Kraków.</p>
             <p>Doskonały do multimedialnego zwiedzania audioprzewodnika w formie audioprzewodnika za pomocą aplikacji multimedialnej. Nie czekaj ani chwili dłużej, zamów audioprzewodnik w formie audioprzewonika już dziś!</p>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
