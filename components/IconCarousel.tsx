dec1d70a4e7127bc8d24569f9a8ac7ab981aa16a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-08-27 19:06:48 +0200

    update Icon carousel

--- components/IconCarousel.tsx
+++ components/IconCarousel.tsx
@@ -5,17 +5,19 @@ const Carousel: React.FC = () => {
   const duplicatedIcons = [...appCarouselIcons, ...appCarouselIcons];
 
   return (
-    <div className="carousel-container relative bg-white">
-      <div className="animate-scroll whitespace-nowrap py-2 mt-1">
+    <div className='carouselContainer overflow-hidden w-100 relative bg-white'>
+      <div className='carousel whitespace-nowrap py-2 mt-1 flex w-max'>
+        <div className='track'>
         {duplicatedIcons.map((icon, index) => (
           <div key={index} className="inline-block px-4">
             <img
               src={`/img/carousel-icons/app-carousel-icon-${icon.id}.png`}
               alt={icon.alt}
-              className="carousel-icon h-6 md:h-8"
+              className='carousel-icon h-6 md:h-8'
             />
           </div>
         ))}
+        </div>
       </div>
     </div>
   );
