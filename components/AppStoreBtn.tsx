89a05a972fbe4fd8a5c3133b86c23eac52de7a21
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-08-29 14:43:18 +0200

    update app icons layout

--- components/AppStoreBtn.tsx
+++ components/AppStoreBtn.tsx
@@ -11,7 +11,9 @@ type AppStoreBtnProps = {
 const AppStoreBtn = ({ btnColor, iconSrc, storeName, }: AppStoreBtnProps) => {
   return (
     <Button className={`flex justify-between ${btnColor} rounded-full w-24 md:w-36 h-6 md:h-8 px-2`}>
+      <div>
         <Image src={`/img/app-icons/${iconSrc}`} alt="App store Icon" width={14} height={14} className='hidden md:block'/>
+      </div>
         <p className='text-[10px] md:text-sm md:font-bold'>{storeName}</p>
         <Image src="/img/app-icons/External_Link_Icon.svg" alt="External link Icon" width={14} height={14} />
     </Button>
--- components/FavouriteAppCard.tsx
+++ components/FavouriteAppCard.tsx
@@ -10,14 +10,16 @@ type FavouriteAppsCardProps = {
 const FavouriteAppCard = ({fileName, placeName, rating}: FavouriteAppsCardProps) => {
   return (
     <div className='w-[340px] md:w-[500px] md:h-[104px] flex items-center bg-white rounded-2xl drop-shadow-md px-2'>
-      <div className="ms-3">
-        <Image src={`/img/app-icons/${fileName}`} width={80} height={80} className='rounded-lg' alt='Ikona ulubionej aplikacji' />
+      <div className="flex w-[60px] h-[60px] ms-3">
+        <Image src={`/img/app-icons/${fileName}`} width={80} height={80} className='rounded-lg max-w-none' alt='Ikona ulubionej aplikacji' />
       </div>
       <div className="m-3 flex flex-col leading-5 justify-center gap-1 ps-1">
         <h5 className='text-sm md:text-text2xl font-bold'>{placeName}</h5>
         <div className="flex items-center gap-2">
-          <Image src="/img/star.png" width={18} height={18} alt="rating" />
-          <p className="text-lg font-bold text-amber-400">{rating}</p>
+          <div className="w-4 h-4">
+            <Image src="/img/star.png" width={18} height={18} alt="rating" />
+          </div>
+          <p className="text-sm md:text-lg font-bold text-amber-400">{rating}</p>
         </div>
       </div>
       <div className="flex flex-col gap-2 justify-between">
