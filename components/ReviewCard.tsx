4c6a505eaad8c0d988c730e706aefed1d670840c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2024-08-30 12:16:06 +0200

    remove img from reviewcard

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -14,7 +14,7 @@ const ReviewCard = ({ name, isImgLeft, profilePic, place, desc, rating }: Review
   return (
     <div className='relative w-72 h-auto bg-white shadow-xl rounded-lg my-2 py-5 ps-10 pe-6 leading-loose'>
         <h3 className='text-sm font-bold'>{name}</h3>
-          <Image src={profilePic} width={45} height={45} alt='Profile img' className={`absolute rounded-full shadow-lg top-4 ${isImgLeft ? '-left-4' : '-right-4'}`}/>
+          {/* <Image src={profilePic} width={45} height={45} alt='Profile img' className={`absolute rounded-full shadow-lg top-4 ${isImgLeft ? '-left-4' : '-right-4'}`}/> */}
         <p className='text-[0.6rem] text-muted-foreground'>{place}</p>
         <h5 className='text-xs'>{desc}</h5>
         <div className="flex items-center gap-1 mt-1">
